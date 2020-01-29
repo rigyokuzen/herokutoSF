a962abff3e7e8dbca080a13f57c92128cbe21e47
rigyokuzen <liyq@su-go.co.jp>
2020-01-29 17:15:26 +0900

    test

--- index.php
+++ index.php
@@ -8,6 +8,12 @@ $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1)
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
 var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
+
+$conn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1) . ';user=' .$dbinfo['user']. ';password=' .$dbinfo['pass'];
+$link = pg_connect($conn);
+if (!$link) {
+    die('接続失敗です。'.pg_last_error());
+}
 $result = pg_query('SELECT * FROM users where email =\'hina3588@bbap.ll\'');
 if (!$result) {
     die('クエリーが失敗しました。'.pg_last_error());
