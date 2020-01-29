19963bafe554583cc001a115ca0405b2c7785ce9
rigyokuzen <liyq@su-go.co.jp>
2020-01-29 17:07:03 +0900

    select user

--- index.php
+++ index.php
@@ -8,4 +8,9 @@ $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1)
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
 var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
+$result = pg_query('SELECT * FROM users where email =\'hina3588@bbap.ll\'');
+if (!$result) {
+    die('クエリーが失敗しました。'.pg_last_error());
+}
+var_dump($result);
 ?>
